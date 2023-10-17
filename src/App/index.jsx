76f18c6ab232f1d7763a5d82aaa29d0949b4b8a6
Juan Ospina <zuleta2411@hotmail.com>
2023-10-17 18:22:07 -0500

    change turn when press a square

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -5,23 +5,31 @@ import Square from "../Square"
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
   return (
     <div className="flex flex-col items-center gap-5 mt-32">
-      <h1 className="text-white text-4xl font-semibold">Tic Tac Toe</h1>
+      <h1 className="text-white text-5xl font-semibold">Tic Tac Toe</h1>
       <section className="grid grid-cols-3 grid-rows-3 gap-x-3 gap-y-2 text-white">
         {
           board.map((_, index) => {
             return (
-              <Square key={index} index={index}>
+              <Square key={index} index={index} updateBoard={updateBoard}>
                 {board[index]}
               </Square>
             )
           })
         }
       </section>
-      <section className="flex gap-4 text-white text-7xl">
-        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
-        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      <section className="flex flex-col items-center gap-4 text-white mt-10">
+        <h1 className="font-semibold text-4xl">Turn</h1>
+        <div className= "text-7xl flex gap-4">
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        </div>
       </section>
     </div>
   )
--- src/Square/index.jsx
+++ src/Square/index.jsx
@@ -1,7 +1,10 @@
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const style = `border-2 border-white flex justify-center rounded-lg w-28 h-28 cursor-pointer ${isSelected ? 'bg-white text-black' : ''}`
+  const handleClick = () => {
+    updateBoard()
+  }
   return (
-    <div className={style}>
+    <div onClick={handleClick} className={style}>
       {children}
     </div>
   );
